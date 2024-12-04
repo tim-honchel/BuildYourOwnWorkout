265bd2d2a9dca043a5cb3832e54f6794e143de1a
Tim Honchel <94769316+tim-honchel@users.noreply.github.com>
2024-12-04 15:24:46 -0500

    set up endpoints for main controller

--- Workout/Presentation.Api/Controllers/MainController.cs
+++ Workout/Presentation.Api/Controllers/MainController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Workouts.Entities.Dto;
+using Workouts.Logic.Implementations;
 
 namespace Workouts.Api.Controllers
 {
@@ -6,5 +8,76 @@ namespace Workouts.Api.Controllers
     [Route("[controller]")]
     public class MainController : ControllerBase
     {
+        private UserLogic UserLogic { get; set; }
+        private WorkoutLogic WorkoutLogic { get; set; }
+
+        public MainController(UserLogic userLogic, WorkoutLogic workoutLogic) 
+        {
+            UserLogic = userLogic;
+            WorkoutLogic = workoutLogic;
+        }
+
+        // use headers for authentication and authorization
+
+        [HttpGet("CurrentUserId")]
+        public IActionResult OnGetCurrentUserId()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("CurrentUsername")]
+        public IActionResult OnGetUsername()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("Workout/{workoutId}")]
+        public IActionResult OnGetWorkout(long workoutId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("Workouts/{userId}")]
+        public IActionResult OnGetWorkouts(long userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("AddWorkout")]
+        public IActionResult OnPostAddWorkout([FromBody]WorkoutDto workout)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("ValidateWorkout")]
+        public IActionResult OnPostValidateWorkout([FromBody]WorkoutDto workout)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("ArchiveWorkout/{workoutId}")]
+        public IActionResult OnPutArchiveWorkout(long workoutId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("UnarchiveWorkout/{workoutId}")]
+        public IActionResult OnPutUnarchiveWorkout(long workoutId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("UpdateUsername/{username}")]
+        public IActionResult OnPutUpdateUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("UpdateWorkout")]
+        public IActionResult OnPutUpdateWorkout([FromBody]WorkoutDto workout)
+        {
+            throw new NotImplementedException(); 
+        }
+        
     }
 }
