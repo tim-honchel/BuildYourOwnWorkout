8bbb9dccb394fbe6e50f26e453617bc4e2b24ed1
Tim Honchel <94769316+tim-honchel@users.noreply.github.com>
2024-12-07 16:36:44 -0500

    set up API call service

--- Workout/Workout.Presentation/Services/IApiCallService.cs
+++ Workout/Workout.Presentation/Services/IApiCallService.cs
@@ -1,6 +1,18 @@
-﻿namespace Workouts.Presentation.Services
+﻿using Workouts.Entities.Dto;
+
+namespace Workouts.Presentation.Services
 {
     public interface IApiCallService
     {
+        long GetUserId();
+        long GetUsername();
+        WorkoutDto GetWorkout(long workoutId);
+        List<WorkoutDto> GetWorkouts(long userId);
+        long AddWorkout(WorkoutDto workout);
+        void ArchiveWorkout(long workoutId);
+        void UnarchiveWorkout(long workoutId);
+        void UpdateUsername(string username);
+        void UpdateWorkout(WorkoutDto workout);
+
     }
 }
