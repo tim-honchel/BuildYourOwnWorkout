cf6dd9dd57bad9a4fdc536118a6d8d47d91d1d4d
Tim Honchel <94769316+tim-honchel@users.noreply.github.com>
2024-12-10 21:21:50 -0500

    set up NavMenu code behind and write unit tests

--- Workouts.Presentation/Components/Shared/NavMenu.razor.cs
+++ Workouts.Presentation/Components/Shared/NavMenu.razor.cs
@@ -1,6 +1,24 @@
+using System.Runtime.CompilerServices;
+using Workouts.Presentation.Services;
+
+[assembly: InternalsVisibleTo("Workouts.UnitTests")]
 namespace Workouts.Presentation.Components.Shared
 {
     public partial class NavMenu
     {
+        private IApiCallService _service;
+        public NavMenu(IApiCallService service)
+        {
+            _service = service;
+        }
+        protected override async Task OnInitializedAsync()
+        {
+            LoadUserInfo();
+            await base.OnInitializedAsync();
+        }
+
+        internal void LoadUserInfo()
+        {
+        }
     }
 }
---
+++ Workouts.UnitTests/Presentation/NavMenuTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Moq;
+using Workouts.Presentation.Components.Shared;
+using Workouts.Presentation.Services;
+
+namespace Workouts.UnitTests.Presentation
+{
+    public class NavMenuTests : IDisposable
+    {
+        private Mock<IApiCallService> _mockService;
+        public NavMenuTests() 
+        {
+            _mockService = new Mock<IApiCallService>();
+        }
+        public void Dispose()
+        {
+            _mockService.Reset();
+        }
+
+        [Fact]
+        public void OnInitializedAsync_GetsUserId_GivenAuthentication()
+        {
+            var menu = new NavMenu(_mockService.Object);
+
+            menu.LoadUserInfo();
+
+            _mockService.Verify(m => m.GetUserId(), Times.Once());
+        }
+
+        [Fact]
+        public void OnInitializedAsync_GetsUsername_GivenAuthentication()
+        {
+            var menu = new NavMenu(_mockService.Object);
+
+            menu.LoadUserInfo();
+
+            _mockService.Verify(m => m.GetUsername(), Times.Once());
+        }
+
+    }
+}
